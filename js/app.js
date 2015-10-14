00480bfd6a6db07b477efdac002f437b9a2d6bf8
Vitaly Bondar <johngull@gmail.com>
2015-10-14 17:43:00 +0300

    Base class for enemies and Player for rendering

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
+// Base class for Enemies and player
+var Actor = function(x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+Actor.prototype.movePosition = function(dx, dy) {
+    this.x = this.x + dx;
+    this.y = this.y + dy;
+};
+
+Actor.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(row, col) {
+    Actor.call(this, col*101, row*83);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype = Object.create(Actor.prototype);
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,23 +33,17 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(row, col) {
+    Actor.call(this, col*101, row*83);
     this.sprite = "images/char-boy.png";
 };
 
+Player.prototype = Object.create(Actor.prototype);
+
 Player.prototype.update = function(dt) {
-
-};
-
-Player.prototype.render = function() {
 
 };
 
@@ -43,8 +54,8 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-    var allEnemies = [new Enemy()];
-    var player = new Player();
+    var allEnemies = [new Enemy(1,0)];
+    var player = new Player(5,2);
 
 
 // This listens for key presses and sends the keys to your
